8bfa9320edc694c77eba1e49ad034612c5806557
vedrangrcic <42610228+vedrangrcic@users.noreply.github.com>
2019-02-13 01:03:12 +0100

    a

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ip6PcVA9iplGvOudJoidYLcXcllBsH7u'
+export_file_name = 'bestunfrozen.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['altocumulus', 'altostratus', 'cirrocumulus', 'cirrostratus', 'cirrus', 'cumulonimbus', 'cumulus', 'nimbostratus', 'stratocumulus', 'stratus']
 path = Path(__file__).parent
 
 app = Starlette()
